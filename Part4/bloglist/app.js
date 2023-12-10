3d38bc4c05fcbd8736e238e8d7150a3095b26f93
Waseem Akram P <waseemsvm14@gmail.com>
2023-12-10 17:42:15 +0530

    commit exercise upto 4.20

--- Part4/bloglist/app.js
+++ Part4/bloglist/app.js
@@ -4,12 +4,16 @@ const cors = require("cors");
 require("express-async-errors");
 const middleware = require("./utils/middleware");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use("/api/login", loginRouter);
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- Part4/bloglist/controllers/blogs.js
+++ Part4/bloglist/controllers/blogs.js
@@ -1,18 +1,50 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const middleware = require("../utils/middleware");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", ["username", "name"]);
   response.json(blogs);
 });
 
-blogsRouter.post("/", async (request, response) => {
+// const getTokenFrom = (request) => {
+//   const authorization = request.get("authorization");
+//   if (authorization && authorization.startsWith("Bearer ")) {
+//     return authorization.replace("Bearer ", "");
+//   }
+//   return null;
+// };
+
+blogsRouter.post("/", middleware.tokenExtractor, async (request, response) => {
+  // const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  // if (!request.token)
+  //   return response.status(401).json({ error: "token invalid" });
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
+
   if (!(request.body.title && request.body.url)) response.status(400).end();
 
-  const blog = new Blog(request.body);
+  // const blog = new Blog(request.body);
+  const blog = new Blog({
+    author: request.body.author,
+    likes: request.body.likes,
+    title: request.body.title,
+    url: request.body.url,
+    user: user._id,
+  });
 
-  const resutl = await blog.save();
-  response.status(201).json(resutl);
+  const savedBlog = await blog.save();
+
+  user.blogs = user.blogs.concat(savedBlog._id);
+
+  await user.save();
+
+  response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
